a1b21537fad5814097ca94103e8d1a0b2bb09c05
ziadasem <zeyadstudio@gmail.com>
2019-09-08 01:45:07 +0200

    initial commit

---
+++ lib/transaction.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Transaction {
+final String id ;
+final String tittle ;
+final double amount ;
+final DateTime dateTime ;
+
+Transaction({
+this.id ,
+this.tittle,
+this.amount,
+this.dateTime
+});
+
+
+
+}
